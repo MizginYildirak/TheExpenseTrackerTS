755864fa2ae469c05556f64a4187ae5ed971acb1
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2025-03-09 12:35:04 +0300

    Update RecentExpenses.tsx

--- src/screens/RecentExpenses.tsx
+++ src/screens/RecentExpenses.tsx
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import { useContext } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../components/context/expenses-context";
 
 export default function RecentExpenses() {
+  const { expenses } = useContext(ExpensesContext);
+
   return (
-    <View>
-      <Text>RecentExpenses</Text>
-    </View>
-  )
+    <ExpensesOutput
+      expenses={expenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
