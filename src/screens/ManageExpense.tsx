5f7e68b13e67e24babf8bcc7f6a4a6dea939007d
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2025-03-09 12:35:21 +0300

    Update ManageExpense.tsx

--- src/screens/ManageExpense.tsx
+++ src/screens/ManageExpense.tsx
@@ -1,12 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import { useContext, useLayoutEffect } from "react";
+import { ExpensesContext } from "../components/context/expenses-context";
 
-export default function ManageExpense() {
+export default function ManageExpense({route, navigation}) {
+  const expensesCtx = useContext(ExpensesContext);
+
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function confirmHandler(expenseData) {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, expenseData);
+    } else {
+      expensesCtx.addExpense(expenseData);
+    }
+    navigation.goBack();
+  }
+  
+
   return (
     <View>
-      <Text>ManageExpense</Text>
+      <ExpenseForm
+        submitButtonLabel={isEditing ? "Update" : "Add"}
+        onCancel={cancelHandler}
+        onSubmit={confirmHandler}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
