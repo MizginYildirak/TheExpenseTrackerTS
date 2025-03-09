cef2158e0719a94465a177e7944503f316697fc8
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2025-03-09 12:32:11 +0300

    Update ExpenseForm.tsx

--- src/components/ManageExpense/ExpenseForm.tsx
+++ src/components/ManageExpense/ExpenseForm.tsx
@@ -1,12 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import Input from "./Input";
+import Button from "../UI/Button";
 
-export default function ExpenseForm() {
+export default function ExpenseForm({
+  submitButtonLabel,
+  onCancel,
+  onSubmit,
+  defaultValues,
+}) {
   return (
     <View>
       <Text>ExpenseForm</Text>
+      <View>
+        <View>
+          <Input />
+          <Input />
+        </View>
+        <Input />
+      </View>
+      <View>
+        <Button onPress={onCancel}>Cancel</Button>
+        <Button onPress={onSubmit}>{submitButtonLabel}</Button>
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
